061a4a169109cd13016b8daf95ce53182b435016
Raymond Muller <raymond.muller.1989@gmail.com>
2020-09-10 11:40:32 +0200

    add laravel 8

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -72,6 +72,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             case 5:
             case 6:
             case 7:
+            case 8:
               return new ServiceProviderLaravel5($app);
 
             default:
